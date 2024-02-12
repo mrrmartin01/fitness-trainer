4af00d8696448c6a88e92fa936c4d7d6b33eab4e
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-12 01:49:06 +0000

    intializing the dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-       <h1 className="text-3xl font-bold underline text-red-400">
+       <h1 className="text-3xl font-bold underline text-blue-500">
       Hello world!
     </h1>
     </>
