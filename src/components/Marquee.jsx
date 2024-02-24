c39a370b3507e46f47a0b634a9258431e8b9135b
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-24 01:02:40 +0000

    fixing marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,43 +1,45 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Rows from "./Rows";
-import WorkOutCard from "./WorkOutCard";
-// import WorkoutsData from "../data/WorkoutsData";
+import WorkOutCard from "./WorkCard";
+import WorkoutsData from "..//WorkoutsData";
 
 function Marquee() {
   return (
     <>
       <WorkOutCard
         className={
-          "flex flex-row items-center gap-2 w-[40%] h-[40%] bg-red-300"
+          "flex flexrow items-center gap-2 w-[40%] h-[40%] bg--500"
         }
       >
-        <Rows
-          value={
-            <>
-              <p className="text-[#ea580c]">sdrfg</p>
-            </>
-          }
-        />
-        <Rows
-          value={
-            <>
-              <p className="text-[#ea580c]">sdrfg</p>
-            </>
-          }
-        />
+        <div className=" flex-wrap-reverse gap-2">
+          {WorkoutsData.map((workouts) => (
+            <Rows
+              key={workouts._id}
+              value={
+                <>
+                  <p className="text-[#ea580c] bg-blue-300">{workouts.type}</p>
+                </>
+              }
+            />
+          ))}
+        </div>
       </WorkOutCard>
 
-      {/* {WorkoutsData.map((workouts) => (
-        <marquee key={workouts._id} className="">
-          <Link to={workouts.url} className="flex flex-row items-center gap-2">
+      <marquee className="">
+        {WorkoutsData.map((workouts) => (
+          <Link
+            key={workouts._id}
+            to={workouts.url}
+            className="flex flex-row items-center gap-2"
+          >
             <img src={workouts.img} alt={workouts.type} className="w-16 h-16" />
             <div className="flex flex-col">
               <h1 className="text-xl font-bold">{workouts.type}</h1>
               <p className="text-gray-700">{workouts.intro}</p>
             </div>
           </Link>
-        </marquee>
-      ))} */}
+        ))}
+      </marquee>
     </>
   );
 }
