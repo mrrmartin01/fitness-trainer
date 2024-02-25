82f4c7cf849227f2c0e329c6647c56191315dbe6
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-25 04:50:22 +0000

    settings page editing intialized

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,42 @@
+import Button from "../components/Button";
+import ProfilePic from "../components/ProfilePic";
+import { IoCloudUploadOutline } from "react-icons/io5";
+import { IoSync } from "react-icons/io5";
+import { FaCrown } from "react-icons/fa";
 
 function Settings() {
   return (
-    <div>
-      Settings
-    </div>
-  )
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <ProfilePic />
+        <Button
+          icon={
+            <span className="flex flex-start items-center justify-center">
+              <IoCloudUploadOutline className="h-7 w-7 pl-1 btnIicons" />
+            </span>
+          }
+          value="Sync Data"
+          type="button"
+          className="w-[30%] h-12 rounded-full p-2 bg-gradient-to-br from-blue-400 to-blue-600 text-white text-[20px] font-serif flex flex-row  justify-center m-auto"
+        />
+        <div className="flex flex-row items-center justify-center ">
+          <IoSync className="text-4xl mr-2 pt-4 text-blue-500" />
+          <p className="mt-4 text-xl">Last Sync:
+          <span>Today 11:30pm</span></p>
+        </div>
+        <Button
+          icon={
+            <span className="flex flex-start items-center justify-center">
+              <FaCrown className="h-7 w-7 pl-1 btnIicons" />
+            </span>
+          }
+          value="Sync Data"
+          type="button"
+          className="w-[30%] h-12 rounded-full p-2 bg-gradient-to-br from-yellow-500 to-yellow-600 text-white text-[20px] font-serif flex flex-row  justify-center m-auto"
+        />
+      </div>
+    </>
+  );
 }
 
-export default Settings
+export default Settings;
