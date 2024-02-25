d9d3ec6a83cab00a234728692fa7d6ae86c52136
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-25 03:00:18 +0000

    nav fixed

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import NavData from "../data/NavListData";
 import { Link } from "react-router-dom";
 import ProfilePic from "./ProfilePic";
 
 function NavList() {
+  const [navItems, setNavItems] = useState(NavData);
+
+  const handleClick = (id) => {
+    const updatedNavItems = navItems.map((item) =>
+      item._id === id ? { ...item, active: true } : { ...item, active: false }
+    );
+    setNavItems(updatedNavItems);
+  };
+
   return (
     <>
-    <ProfilePic />
-      {NavData.map((item) => (
+      <ProfilePic />
+      {navItems.map((item) => (
         <li key={item._id} className="">
           <Link
             to={item.url}
-            className={`flex gap-1 items-center hover:bg-gray-500 rounded-lg p-4 
-            ${item.active?  "bg-gray-500" : ""}
-            `}
+            className={`flex gap-1 items-center hover:bg-gray-500 rounded-lg p-4 ${
+              item.active ? "bg-gray-500" : ""
+            }`}
+            onClick={() => handleClick(item._id)}
           >
             {item.icon && <item.icon className="mr-4" />}
             <span>{item.name}</span>
