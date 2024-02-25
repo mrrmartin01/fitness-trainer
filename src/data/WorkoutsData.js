71af7e6eb5e19b527912cdfd6e96b225483e206d
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-25 02:36:53 +0000

    card fixed

--- src/data/WorkoutsData.js
+++ src/data/WorkoutsData.js
@@ -5,7 +5,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/",
+        'img': "https://www.fitness-books.com/wp-content/uploads/2017/04/Mens-Ab-Workout-356x220.jpg",
     },
     {
         'id': 2,
@@ -13,7 +13,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://cdn.exercise.com/images/4111/ios_large.png"
     },
     {
         'id': 3,
@@ -21,7 +21,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://steelsupplements.com/cdn/shop/articles/shutterstock_1255020979_2000x.jpg?v=1604529149"
     },
     {
         'id': 4,
@@ -29,7 +29,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://cdn.shopify.com/s/files/1/1497/9682/files/1.Benefits_of_Having_a_Strong_Chest.jpg?v=1683545634&width=750"
     },
     {
         'id': 5,
@@ -37,12 +37,15 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://www.setforset.com/cdn/shop/articles/back_and_chest_workout_2000x.jpg?v=1660093535"
     },
     {
         'id': 6,
         'type': "Chest-Expert",
-        'url': '', 'intro': "For absolute beginners looking forward to training their abs", 'detail': "", 'img': "../assets/img/"
+        'url': '', 
+        'intro': "For absolute beginners looking forward to training their abs", 
+        'detail': "", 
+        'img': "https://i0.wp.com/www.muscleandfitness.com/wp-content/uploads/2014/12/chest-proper-pec-pounding-promo.jpg?quality=86&strip=all"
     },
     {
         'id': 7,
@@ -50,7 +53,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://www.bodybuilding.com/images/2021/april/shredded-legs-for-summer-tall.jpg"
     },
     {
 
@@ -59,7 +62,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://media.istockphoto.com/id/503147349/photo/never-skip-leg-day.jpg?s=612x612&w=0&k=20&c=1EBhIkK9gggeGVSgS4hQDGBxuhhAIndPksBBeHTg2Zo="
     },
     {
         'id': 9,
@@ -67,7 +70,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://steelsupplements.com/cdn/shop/articles/shutterstock_347383067_1000x.jpg?v=1626087187"
     },
     {
         'id': 10,
@@ -75,7 +78,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://cdn.shopify.com/s/files/1/0090/4773/6378/files/The_best_tricep_exercise.jpg?v=1603396597"
     },
     {
         'id': 11,
@@ -83,7 +86,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://www.bodybuilding.com/images/2019/september/30day-arms-square-1200x1200-b.jpg"
     },
     {
         'id': 12,
@@ -91,7 +94,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://generationiron.com/wp-content/uploads/2021/10/534B6178-9581-4BD6-8D73-C72D834E23AB.jpeg"
     },
     {
         'id': 13,
@@ -99,7 +102,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://image.boxrox.com/2021/11/Back-Muscles-2.jpg"
     },
     {
         'id': 14,
@@ -107,7 +110,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://image.boxrox.com/2021/09/Best-Rear-Delt-Exercises.jpg"
     },
     {
         'id': 15,
@@ -115,7 +118,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://miro.medium.com/v2/resize:fit:1013/1*yNLZNxzUl57Vdr7dYYx9WQ.jpeg"
     },
     {
         'id': 16,
@@ -123,7 +126,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://www.aleanlife.com/wp-content/uploads/2015/04/quick-home-back-workout.jpg"
     },
     {
         'id': 17,
@@ -131,7 +134,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://cdn.muscleandstrength.com/sites/default/files/back800.jpg"
     },
     {
         'id': 18,
@@ -139,7 +142,7 @@ const WorkoutsData = [
         'url': '',
         'intro': "For absolute beginners looking forward to training their abs",
         'detail': "",
-        'img': "../assets/img/"
+        'img': "https://www.ericfavre.com/lifestyle/uk/wp-content/uploads/sites/15/2021/09/augmentez-la-largeur-du-dos-1024x593-1.jpg"
     },
 ]
 
