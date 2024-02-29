bfcfeb8d8cb70ea734c3df69e69e5669ce4e13f0
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-02-29 13:02:15 +0000

    adding more models to the homepage

---
+++ src/components/LevelsCard.jsx
@@ -0,0 +1,16 @@
+
+function LevelsCard() {
+  return (
+    <div className="w-full bg-gray-600 rounded-lg flex flex-row justify-between">
+        <span>
+            dhke
+        </span>
+        <span>
+            {}
+        </span>
+      
+    </div>
+  )
+}
+
+export default LevelsCard
