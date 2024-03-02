047edc1806aee31db21a7ce9109f4b902a06148e
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-03-02 17:59:50 +0000

    levels card

--- src/components/LevelsCard.jsx
+++ src/components/LevelsCard.jsx
@@ -1,22 +1,44 @@
 import PropTypes from "prop-types";
 
-function LevelsCard({ name, image , bg}) {
+function LevelsCard({ name, image, bg }) {
   return (
     <>
-    <div className="w-full h-[200px] rounded-lg flex flex-row justify-between mb-2">
-      <div
-        className="flex flex-col gap-2 p-2 w-[50%] bg-center bg-cover rounded-l-lg"
-        style={{ backgroundImage: `url(${bg})`, opacity: 0.7 }}
-      >
-        <span className="font-semibold text-[500%] font-mono italic">5 Workouts</span>
-        <span className="text-[200%] font-serif">{name}</span>
+      <div className="w-full h-[200px] rounded-lg flex flex-row justify-between mb-2 border-2 border-sky-300">
+        <div
+          className="flex flex-col gap-2 p-2 w-[50%] bg-center bg-cover rounded-l-lg"
+          style={{ backgroundImage: `url(${bg})`, opacity: 0.7 }}
+        >
+          <span className="font-semibold text-[500%] font-mono italic">
+            5 Workouts
+          </span>
+          <span className="text-[200%] font-serif">{name}</span>
+        </div>
+
+        <div className="w-[50%]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-r-lg opacity-70"
+          />
+        </div>
       </div>
 
-      <div className="w-[50%]">
-        <img src={image} alt={name} className="w-full h-full object-cover rounded-r-lg opacity-70" />
+      <div className="w-full h-[100px] border-b-2 border-red-400 bg-blue-500 mb-2">
+        <div className="flex items-center justify-between p-3 h-full">
+          <div className="w-[50%] h-full">
+            <img
+              src={image}
+              alt={name}
+              className="w-full h-full object-cover rounded-r-lg opacity-70"
+              style={{ maxWidth: "100%", maxHeight: "100%" }}
+            />
+          </div>
+          <div className="flex flex-col justify-center flex-grow mx-3">
+            <span className="text-white font-medium">{name}</span>
+          </div>
+            <span className="text-white opacity-70">--</span>
+        </div>
       </div>
-    </div>
-
     </>
   );
 }
--- src/data/LevelsCards.js
+++ src/data/LevelsCards.js
@@ -1,7 +1,28 @@
 const LevelsCards = [
-    { id: 1, bg_img: 'https://img.pikbest.com/wp/202344/rough-texture-gradient-black-a-seamless-of-cement-wall-with-blackened-surface_9936446.jpg!w700wp', name: 'Beginner', img: 'https://designbigger.com/wp-content/uploads/2023/05/Fat-Guy-Training-Abs.jpg' },
-    { id: 2, bg_img: 'https://e0.pxfuel.com/wallpapers/488/9/desktop-wallpaper-texture-gradient-full-wzxoq-grey.jpg', name: 'Intermediate', img: 'https://www.dmoose.com/cdn/shop/articles/feature-image_5437ab6a-8565-46da-bb72-6891b21a95bc.jpg?v=1685102059' },
-    { id: 3, bg_img: 'https://wallpapers.com/images/hd/gray-gradient-background-iusev9u2883edgkd.jpg', name: 'Advanced', img: 'https://d3h9ln6psucegz.cloudfront.net/wp-content/uploads/2017/06/Full-Body-Training-For-Advanced-Lifters.jpg' }
+    {
+        id: 1,
+        bg_img: 'https://img.pikbest.com/wp/202344/rough-texture-gradient-black-a-seamless-of-cement-wall-with-blackened-surface_9936446.jpg!w700wp',
+        name: 'Beginner',
+        img: 'https://designbigger.com/wp-content/uploads/2023/05/Fat-Guy-Training-Abs.jpg',
+        [{type: 'arms' , name: '' , width: '300px', height: '' }]
+
+    : 'https'},
+
+
+    {
+        id: 2,
+        bg_img: 'https://e0.pxfuel.com/wallpapers/488/9/desktop-wallpaper-texture-gradient-full-wzxoq-grey.jpg',
+        name: 'Intermediate',
+        img: 'https://www.dmoose.com/cdn/shop/articles/feature-image_5437ab6a-8565-46da-bb72-6891b21a95bc.jpg?v=1685102059'
+    },
+
+
+    {
+        id: 3,
+        bg_img: 'https://wallpapers.com/images/hd/gray-gradient-background-iusev9u2883edgkd.jpg',
+        name: 'Advanced',
+        img: 'https://d3h9ln6psucegz.cloudfront.net/wp-content/uploads/2017/06/Full-Body-Training-For-Advanced-Lifters.jpg'
+    }
 ]
 
 
