a13b8f47fcd9b66329dc122cc5560f7df1d59f14
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-03-08 17:51:05 +0000

    json files 2

---
+++ src/components/workouts/IntermediateAbs.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+
+function IntermediateAbs(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+IntermediateAbs.propTypes = {
+
+}
+
+export default IntermediateAbs
+
